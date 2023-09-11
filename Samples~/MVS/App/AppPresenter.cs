ac900b91744e2a4658bc16383df86904e2132a2a
cho.kinken <mahaweilu@gmail.com>
2023-09-11 13:38:47 +0900

    fix: add suppress message, namespace in Samples

--- Samples~/MVS/App/AppPresenter.cs
+++ Samples~/MVS/App/AppPresenter.cs
@@ -1,4 +1,6 @@
-﻿using Extreal.Core.StageNavigation;
+﻿using System.Diagnostics.CodeAnalysis;
+using Cysharp.Threading.Tasks;
+using Extreal.Core.StageNavigation;
 using VContainer.Unity;
 
 namespace Extreal.Integration.Web.Common.MVS.App
@@ -7,6 +9,7 @@ namespace Extreal.Integration.Web.Common.MVS.App
     {
         private readonly StageNavigator<StageName, SceneName> stageNavigator;
 
+        [SuppressMessage("CodeCracker", "CC0057")]
         public AppPresenter(StageNavigator<StageName, SceneName> stageNavigator)
             => this.stageNavigator = stageNavigator;
 
