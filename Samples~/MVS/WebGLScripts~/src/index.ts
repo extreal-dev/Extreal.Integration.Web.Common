84f1b10d28e03d2ed46e8ee934b8b429dee2a0ce
sus-yoshikane-t <110364227+sus-yoshikane-t@users.noreply.github.com>
2024-02-21 14:26:19 +0900

    指定した関数呼び出しとコールバックのトレースログを抑制できるように変更 (#7)

--- Samples~/MVS/WebGLScripts~/src/index.ts
+++ Samples~/MVS/WebGLScripts~/src/index.ts
@@ -7,3 +7,16 @@ addAction("DoAction", (str1, str2) => {
 addFunction("DoFunction", (str1, str2) => {
     return `received param1=[${str1}] param2=[${str2}] in function`;
 });
+
+addAction("DoTraceLogSuppressedAction",
+    (str1, str2) => {
+        callback("HandleOnCallback", `param1=[${str1}]`, `param2=[${str2}]`, true);
+    },
+    true);
+
+addFunction(
+    "DoTraceLogSuppressedFunction",
+    (str1, str2) => {
+        return `received param1=[${str1}] param2=[${str2}] in function`;
+    },
+    true);
